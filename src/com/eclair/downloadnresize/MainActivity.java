6b509d4a4f6b00db291f9d6a5047fe0f2104d1ab
Andrew Cherkashin <cherkashin.andrey90@gmail.com>
2015-04-26 13:28:20 +0300

    Added widgets binding

--- src/com/eclair/downloadnresize/MainActivity.java
+++ src/com/eclair/downloadnresize/MainActivity.java
@@ -4,12 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private EditText urlField;
+    private ImageButton downloadButton;
+    private ListView tasksListView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        bindWidgets();
+    }
+
+    private void bindWidgets() {
+        urlField = (EditText)findViewById(R.id.urlField);
+        downloadButton = (ImageButton)findViewById(R.id.downloadButton);
+        tasksListView = (ListView)findViewById(R.id.tasksListView);
     }
 
     @Override
